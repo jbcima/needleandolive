c6ddc7e754c49513e9caa80b65c5d967800b0163
Jeremy Cimafonte <me@jeremycimafonte.com>
2024-10-16 01:06:08 -0400

    Styles the bottom footer

--- components/layout/footer-menu.tsx
+++ components/layout/footer-menu.tsx
@@ -19,9 +19,10 @@ export function FooterMenuItem({ item }: { item: Menu }) {
       <Link
         href={item.path}
         className={clsx(
-          'block p-2 text-lg underline-offset-4 hover:text-black hover:underline md:inline-block md:text-sm dark:hover:text-neutral-300',
+          'block py-2 text-sm underline-offset-4 hover:text-black hover:underline md:inline-block md:text-sm dark:hover:text-neutral-300',
           {
-            'text-black dark:text-neutral-300': active
+            'text-black dark:text-neutral-300': active,
+            'text-neutral-500 dark:text-neutral-400': !active
           }
         )}
       >
@@ -36,10 +37,15 @@ export default function FooterMenu({ menu }: { menu: Menu[] }) {
 
   return (
     <nav>
-      <ul>
-        {menu.map((item: Menu) => {
-          return <FooterMenuItem key={item.title} item={item} />;
-        })}
+      <ul className="flex flex-col items-center md:flex-row">
+        {menu.map((item: Menu, index) => (
+          <span key={item.title} className="flex items-center">
+            <FooterMenuItem item={item} />
+            {index < menu.length - 1 && (
+              <span className="mx-2 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
+            )}
+          </span>
+        ))}
       </ul>
     </nav>
   );
--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,7 +1,6 @@
 import CartModal from 'components/cart/modal';
 import LogoSquare from 'components/logo-square';
 import { getMenu } from 'lib/shopify';
-import { Menu } from 'lib/shopify/types';
 import Link from 'next/link';
 import { Suspense } from 'react';
 import MobileMenu from './mobile-menu';
@@ -26,21 +25,6 @@ export async function Navbar() {
         >
           <LogoSquare />
         </Link>
-        {menu.length ? (
-          <ul className="hidden gap-6 text-sm md:flex md:items-center">
-            {menu.map((item: Menu) => (
-              <li key={item.title}>
-                <Link
-                  href={item.path}
-                  prefetch={true}
-                  className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-                >
-                  {item.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        ) : null}
       </div>
       <div className="flex justify-end md:absolute md:right-6">
         <CartModal />
