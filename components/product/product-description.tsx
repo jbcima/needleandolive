6810cae4030d28c4ada76555ac18c2a0b6cdb2a7
Jeremy Cimafonte <me@jeremycimafonte.com>
2024-10-16 10:11:11 -0400

    Adds add to cart button if there aren't any variants

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -1,9 +1,12 @@
+import { AddToCart } from 'components/cart/add-to-cart';
 import Price from 'components/price';
 import Prose from 'components/prose';
 import { Product } from 'lib/shopify/types';
 import { VariantSelector } from './variant-selector';
 
 export function ProductDescription({ product }: { product: Product }) {
+  const hasVariants = product.variants.length > 1;
+
   return (
     <>
       <div className="mb-6 flex flex-col">
@@ -15,11 +18,15 @@ export function ProductDescription({ product }: { product: Product }) {
           />
         </div>
       </div>
-      <VariantSelector 
-        product={product}
-        options={product.options} 
-        variants={product.variants} 
-      />
+      {hasVariants ? (
+        <VariantSelector 
+          product={product}
+          options={product.options} 
+          variants={product.variants} 
+        />
+      ) : (
+        <AddToCart product={product} />
+      )}
       {product.descriptionHtml ? (
         <Prose
           className="mb-6 text-sm leading-tight dark:text-white/[60%]"
