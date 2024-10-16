def3972e2cd66827bae508410ff7da28d11eae70
Jeremy Cimafonte <me@jeremycimafonte.com>
2024-10-16 00:02:24 -0400

    Adds ProductImage component for displaying product images

--- components/list/list-product.tsx
+++ components/list/list-product.tsx
@@ -1,4 +1,4 @@
-import { GridTileImage } from 'components/grid/tile'
+import { ProductImage } from 'components/image/product-image'
 import { ProductProvider } from 'components/product/product-context'
 import { ProductDescription } from 'components/product/product-description'
 import { Product } from 'lib/shopify/types'
@@ -7,18 +7,16 @@ import { Suspense } from 'react'
 export default function ListProduct({ product }: { product: Product }) {
   return (
     <ProductProvider>
-      <div className="flex flex-col gap-8 md:flex-row md:gap-12">
-        <div className="w-full md:w-1/2">
-          <div className="relative aspect-square">
-            <GridTileImage
-              src={product.featuredImage?.url}
-              alt={product.title}
-              fill
-              sizes="(min-width: 1024px) 50vw, 100vw"
-            />
-          </div>
+      <div className="flex flex-col gap-4 w-full max-w-2xl mx-auto">
+        <div className="w-full aspect-square relative">
+          <ProductImage
+            src={product.featuredImage?.url}
+            alt={product.title}
+            fill
+            sizes="(min-width: 1024px) 66vw, 100vw"
+          />
         </div>
-        <div className="w-full md:w-1/2">
+        <div className="w-full">
           <Suspense fallback={<div>Loading...</div>}>
             <ProductDescription product={product} />
           </Suspense>
